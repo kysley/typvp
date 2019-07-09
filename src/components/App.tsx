0cabfe3ff52f2a7d604d61d58c36fcd37559d131
Evan Kysley <ekysley@adparlor.com>
2019-07-09 12:55:11 -0400

    feat: add some fonts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,43 +2,42 @@ import React, {FC, Suspense} from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {createGlobalStyle} from 'styled-components'
 
+import IR from '@/assets/fonts/Inter-Regular.woff2'
 import {StoreProvider} from '@/stores'
 import {ClientProvider} from '@/services/Client'
 import Routes from '@/routes'
 
+// TODO: replace with a css import file (and loaders.. etc)
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Manrope';
-    src: url('../assets/fonts/manrope-extrabold.woff2') format('woff2');
+  /* @font-face {
+    font-family: 'Inter';
     font-style: normal;
-    font-weight: 800;
+    font-weight: 900;
+    src: url('../assets/fonts/Inter-Black.woff2') format('woff2'),
+      url('../assets/fonts/Inter-Black.woff') format('woff');
   }
   @font-face {
-    font-family: 'Manrope';
-    src: url('../assets/fonts/manrope-bold.woff2') format('woff2');
+    font-family: 'Inter';
     font-style: normal;
     font-weight: 700;
-  }
+    src: url('../assets/fonts/Inter-Bold.woff2') format('woff2'),
+      url('../assets/fonts/Inter-Bold.woff') format('woff');
+  } */
   @font-face {
-    font-family: 'Manrope';
-    src: url('../assets/fonts/manrope-medium.woff2') format('woff2');
-    font-style: normal;
-    font-weight: 500;
-  }
-  @font-face {
-    font-family: 'Manrope';
-    src: url('../assets/fonts/manrope-regular.woff2') format('woff2');
+    font-family: 'Inter';
     font-style: normal;
     font-weight: 400;
+    src: url(${IR}) format('woff2');
+      /* url('../assets/fonts/Inter-Regular.woff') format('woff'); */
   }
   * {
     box-sizing: border-box;
   }
   body {
-    font-family: 'Manrope', sans-serif;
+    font-family: 'Inter', sans-serif;
     letter-spacing: auto;
     line-height: normal;
-    font-weight: 300;
+    font-weight: 400;
     margin: 0;
     padding: 0;
     min-width: 100vw;
@@ -52,8 +51,8 @@ const GlobalStyle = createGlobalStyle`
 const App: FC = () => (
   <ClientProvider>
     <StoreProvider>
+      <GlobalStyle />
       <Router>
-        <GlobalStyle />
         <Suspense fallback={'Loading ...'}>
           <Routes />
         </Suspense>
--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -1,1 +1,2 @@
 declare module 'random-words'
+declare module '@/assets/fonts/Inter-Regular.woff2'
