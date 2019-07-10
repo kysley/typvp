2981822ca48c031386fac9995b72bd3e95aa66db
Evan Kysley <evan@kysley.com>
2019-07-10 14:09:29 -0400

    feat: better calculation of raw/cpm

--- src/stores/Game.ts
+++ src/stores/Game.ts
@@ -6,7 +6,7 @@ import {TypingState} from '@/types/game'
 let timeout: any = null
 class GameStore {
   @observable
-  typedHistory: string[] = new Array(250).fill('')
+  typedHistory: string[] = new Array(250).fill(null)
 
   @observable
   typedWord: string = ''
@@ -24,6 +24,9 @@ class GameStore {
   cpm: number = 0
 
   @observable
+  rawCpm: number = 0
+
+  @observable
   wpm: number = 0
 
   @observable
@@ -38,15 +41,38 @@ class GameStore {
   @observable
   correct: number = 0
 
-  @action
-  calculateResults(): any {
-    const characters = this.typedHistory
-      .map(word => (word !== '' ? word.length : -1))
-      .reduce((a, b) => a + b + 1, 0)
+  incorrectIndex: number[] = []
 
-    this.cpm = Math.floor((characters * 60) / this.time)
-    this.wpm = (this.cpm - this.corrections) / 5
-    // return Math.floor((characters * 60) / this.time)
+  @action
+  calculateResults = (): any => {
+    /*
+     * 1. Check if the word has been typed, if so, use the length of the typed word.
+     *    if not, we won't want to increase character count (-1)
+     *
+     * 2. -250 being the length of the words array
+     *    not sure what else is going on but it works lol
+     *    +2 is because you count the space as a character as well
+     */
+    const characters = this.typedHistory
+      .map(word => (word !== null ? word.length : -1)) // 1
+      .reduce((a, b) => a + b + 2, -250) // 2
+
+    // Raw CPM => Doesn't include mistakes
+    this.rawCpm = Math.floor((characters / this.time) * 60)
+
+    // Corrected CPM = > Subtract mistakes character length from all characters
+    let adjustCpmBy = 0
+    this.incorrectIndex.forEach(index => {
+      if (this.typedHistory[index] !== null) {
+        adjustCpmBy += this.typedHistory[index].length + 1
+      }
+    })
+
+    const goodCharacters = characters - adjustCpmBy
+
+    this.cpm = Math.floor((goodCharacters / this.time) * 60)
+
+    this.wpm = this.cpm / 5
   }
 
   @action
@@ -58,16 +84,18 @@ class GameStore {
     this.time = 0
     this.typingState = TypingState.NotStarted
     this.cpm = 0
+    this.rawCpm = 0
     this.wpm = 0
     this.generateWords()
     this.corrections = 0
     this.incorrect = 0
     this.correct = 0
+    this.incorrectIndex = []
   }
 
   @action
   generateWords = () => {
-    this.words = randomwords(250)
+    this.words = randomwords({exactly: 250, maxLength: 8})
   }
 
   @action
@@ -107,12 +135,14 @@ class GameStore {
 
       if (this.typingState === TypingState.AwaitingLastWord) {
         this.typingState = TypingState.Finished
+        this.calculateResults()
       }
 
       if (this.typedHistory[this.wordIndex] === this.words[this.wordIndex]) {
         this.correct++
       } else {
         this.incorrect++
+        this.incorrectIndex.push(this.wordIndex)
       }
 
       this.typedWord = ''
