635ff13edc69e71afa775fb7cc5007fab9b2267f
Evan Kysley <evan@kysley.com>
2019-07-14 17:07:13 -0400

    fix: inject font css to head

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,35 +1,12 @@
-import React, {FC, Suspense} from 'react'
+import React, {FC} from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {createGlobalStyle} from 'styled-components'
 
-import IR from '@/assets/fonts/Inter-Regular.woff2'
 import {StoreProvider} from '@/stores'
 import {ClientProvider} from '@/services/Client'
 import Routes from '@/routes'
 
-// TODO: replace with a css import file (and loaders.. etc)
 const GlobalStyle = createGlobalStyle`
-  /* @font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 900;
-    src: url('../assets/fonts/Inter-Black.woff2') format('woff2'),
-      url('../assets/fonts/Inter-Black.woff') format('woff');
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 700;
-    src: url('../assets/fonts/Inter-Bold.woff2') format('woff2'),
-      url('../assets/fonts/Inter-Bold.woff') format('woff');
-  } */
-  @font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 400;
-    src: url(${IR}) format('woff2');
-      /* url('../assets/fonts/Inter-Regular.woff') format('woff'); */
-  }
   * {
     box-sizing: border-box;
   }
