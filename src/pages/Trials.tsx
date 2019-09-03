ce7d9e3470e1cf3226647c9b5d584b29edf139f2
Evan Kysley <evan@kysley.com>
2019-09-03 15:10:26 -0400

    chore: try to dial down Trial Cards styling

--- src/pages/Trials.tsx
+++ src/pages/Trials.tsx
@@ -1,8 +1,15 @@
 import React, {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
 import {useQuery} from 'urql'
 
 import TRIALS from '@/graphql/queries/trials'
-import {TrialCard, TrialCardGrid} from '@/styled/Trials'
+import {
+  TrialCard,
+  TrialCardGrid,
+  TrialName,
+  TrialDifficulty,
+  TrialFold,
+} from '@/styled/Trials'
 
 const Trials = () => {
   const [trials, setTrials] = useState([])
@@ -21,13 +28,19 @@ const Trials = () => {
 
   return (
     <TrialCardGrid>
-      {trials.map((t: any, idx) => (
-        <TrialCard>
-          <p>{t.name}</p>
-          <p>{t.difficulty}</p>
-          <p>{t.minWordLength}</p>
-          <p>{t.maxWordLength}</p>
-        </TrialCard>
+      {trials.map((t: any) => (
+        <Link to={`trials/${t.id}`} key={t.id}>
+          <TrialCard difficulty={t.difficulty}>
+            <TrialName>{t.name}</TrialName>
+            <TrialDifficulty difficulty={t.difficulty}>
+              {t.difficulty}
+            </TrialDifficulty>
+            <TrialFold>
+              <p>min length: {t.minWordLength}</p>
+              <p>max length: {t.maxWordLength}</p>
+            </TrialFold>
+          </TrialCard>
+        </Link>
       ))}
     </TrialCardGrid>
   )
--- src/styled/Trials.ts
+++ src/styled/Trials.ts
@@ -1,14 +1,60 @@
 import styled from 'styled-components'
 
+import {colors} from '@/styled/Theme'
+
 export const TrialCardGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 1em;
+  grid-gap: 2em;
+
+  a {
+    text-decoration: none !important;
+  }
 `
+
+const DifficultyColour = {
+  EASY: colors.g300,
+  NORMAL: colors.b300,
+  MEDIUM: colors.y300,
+  HARD: colors.r300,
+}
 
 export const TrialCard = styled.div`
   border-radius: 6px;
   background: ${({theme}) => theme.backgrounds.accent};
   color: ${({theme}) => theme.colors.text};
   padding: 1em;
+  border-top: 3px solid
+    ${({difficulty}: string) => DifficultyColour[difficulty]};
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+`
+
+export const TrialName = styled.h1`
+  font-weight: 400;
+  font-size: 1.2rem;
+  margin: 0;
+`
+
+export const TrialDifficulty = styled.span`
+  display: inline-block;
+  font-size: 0.82rem;
+  border: 1px solid ${({theme}) => theme.border.default};
+  border-radius: 3px;
+  color: ${({difficulty}: string) => DifficultyColour[difficulty]};
+  padding: 0.25em;
+  margin: 1em 0;
+  font-weight: 700;
+`
+
+export const TrialFold = styled.div`
+  border-top: 1px solid ${({theme}) => theme.border.default};
+  display: flex;
+  justify-content: space-around;
+  padding-top: 0.25em;
+
+  p {
+    font-size: 0.85rem;
+    margin: 0;
+  }
 `
