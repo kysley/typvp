cc60e0fe5eabfda85d9f041f261c576a90181204
Evan Kysley <evan@kysley.com>
2019-11-01 21:10:17 -0400

    feat: improve default colour scheme

--- src/stores/Global.tsx
+++ src/stores/Global.tsx
@@ -2,9 +2,56 @@ import {observable, action} from 'mobx'
 
 import {light, dark} from '@/styled/Theme'
 
+function setColorScheme() {
+  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches
+  const isNotSpecified = window.matchMedia(
+    '(prefers-color-scheme: no-preference)',
+  ).matches
+  const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified
+
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addListener(e => e.matches && handleDefaultMode(true))
+  window
+    .matchMedia('(prefers-color-scheme: light)')
+    .addListener(e => e.matches && handleDefaultMode(false))
+
+  if (isDarkMode) handleDefaultMode(true)
+  if (isLightMode) handleDefaultMode(false)
+  if (isNotSpecified || hasNoSupport) {
+    console.log(
+      'You specified no preference for a color scheme or your browser does not support it. I schedule dark mode during night time.',
+    )
+    // const now = new Date()
+    // const hour = now.getHours()
+    // if (hour < 4 || hour >= 16) {
+    //   handleDefaultMode()
+    // }
+  }
+}
+
+function handleDefaultMode(isDark: boolean): string {
+  const savedMode = localStorage.getItem('mode')
+  const performedDefaultThemeAdjust = localStorage.getItem('pdta')
+  if (performedDefaultThemeAdjust === 'true') return savedMode || 'light'
+
+  if (isDark) {
+    localStorage.setItem('mode', 'dark')
+  } else {
+    localStorage.setItem('mode', 'light')
+  }
+
+  localStorage.setItem('pdta', 'true')
+
+  return localStorage.getItem('mode') as string
+}
+
+setColorScheme()
+
 class GlobalStore {
   @observable
-  mode: string = localStorage.getItem('mode') || 'light'
+  mode: string = localStorage.getItem('mode') as string
 
   @observable
   theme: any = this.mode === 'light' ? light : dark
