e9b87e25f28192f49a560ca8937f2dddf84d1383
Evan Kysley <evan@kysley.com>
2019-11-25 16:26:46 -0500

    feat: Banner component (wip)

---
+++ src/components/Banner.tsx
@@ -0,0 +1,62 @@
+import React, {FC, useState} from 'react'
+import styled from 'styled-components'
+
+import {intentMeta} from '@/styled/Theme'
+
+const BannerWrapper = styled.div`
+  width: 100%;
+  padding: 1em;
+  background-color: ${({theme}) => theme.backgrounds.secondary};
+  border-radius: 4px;
+  margin-bottom: 1em;
+  color: ${({theme}) => theme.colors.text};
+  overflow: hidden;
+  position: relative;
+
+  &::before {
+    display: block;
+    content: '';
+    height: 100%;
+    width: 3px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: ${({intent}: any) => intentMeta[intent].bg};
+  }
+
+  h4 {
+    color: ${({intent}: any) => intentMeta[intent].bg};
+  }
+`
+
+const BannerTitle = styled.h4`
+  font-size: 1.25rem;
+  margin: 0;
+`
+
+const BannerMessage = styled.p`
+  font-size: 1rem;
+  margin-bottom: 0;
+`
+
+interface IBanner {
+  title: string
+  message?: string
+  closeable?: boolean
+  intent?: 'none' | 'success' | 'warning' | 'danger'
+}
+
+export const Banner: FC<IBanner> = ({
+  title,
+  message,
+  closeable = false,
+  intent = 'none',
+}) => {
+  const [open, setOpen] = useState(true)
+  return (
+    <BannerWrapper intent={intent}>
+      <BannerTitle>{title}</BannerTitle>
+      {message && <BannerMessage>{message}</BannerMessage>}
+    </BannerWrapper>
+  )
+}
