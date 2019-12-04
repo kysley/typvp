21818e55b1c2412c563e5bf5c72b6bfe88562be5
Evan Kysley <evan@kysley.com>
2019-12-04 17:01:05 -0500

    feat: basic lobby joining (wip)

--- src/pages/Races.tsx
+++ src/pages/Races.tsx
@@ -1,20 +1,71 @@
 import React, {useEffect, useState} from 'react'
-import io from 'socket.io-client'
 
-const socket = io('http://localhost:8086')
+import {useStore} from '@/stores'
+import {socket} from '@/helpers/socket'
+
+let timeout: any = null
 
 const Race = () => {
-  const [room, setRoom] = useState(null)
+  const {UserStore} = useStore()
+  const [id, setId] = useState(
+    (UserStore.me && UserStore.me.id) ||
+      Math.random()
+        .toString(36)
+        .substring(2, 15),
+  )
+  const [room, setRoom] = useState()
+  const [cpm, setCpm] = useState(0)
+
   useEffect(() => {
     if (!room) {
-      socket.emit('race-find-lobby', {id: 'test'})
+      socket.emit('race-matchmake', {id})
     }
-    socket.on('race-join-lobby', payload => {
+    socket.on('update', payload => {
       console.log(payload)
+      setRoom(payload)
     })
+
+    socket.on('request-race-progress', snapshot => {
+      setRoom(snapshot)
+      socket.emit('race-progress', {cpm})
+    })
+    return () => {
+      socket.removeAllListeners()
+      socket.close()
+    }
   }, [])
 
-  return <span>hey</span>
+  useEffect(() => {
+    // if (room) {
+    //   if (room.state === 'in-progress') {
+    //     // if (room.acceptUpdates) {
+    //     //   // timeout = setTimeout(() => {
+    //     //   socket.emit('race-progress', {cpm})
+    //     //   // }, 1000)
+    //     // }
+    //   } else if (room.state === 'finished') {
+    //     clearTimeout(timeout)
+    //   }
+    //   if (room.acceptUpdates === false) {
+    //     clearTimeout(timeout)
+    //   }
+    // }
+  }, [room])
+
+  return (
+    <div>
+      {room && (
+        <div>
+          <h1>countdown: {room.countdown}</h1>
+          {Object.keys(room.players).map(key => (
+            <h2 key={key}>{room.players[key]}</h2>
+          ))}
+          {room.state}
+          <input onChange={e => setCpm(e.target.value.length)} />
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Race
