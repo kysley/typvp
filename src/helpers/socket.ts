56423137e71fb997d53149f320446c5505a991fe
Evan Kysley <evan@kysley.com>
2019-12-20 17:04:50 -0500

    chore: use appropiate url based on env

--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -1,3 +1,8 @@
 import io from 'socket.io-client'
 
-export const socket = io('http://localhost:8086')
+const socketUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://api.typvp.xyz:8086'
+    : 'http://localhost:8086'
+
+export const socket = io(socketUrl)
