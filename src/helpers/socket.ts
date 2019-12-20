197f00ee08c0996314d8bf05e28c840073b12998
Evan Kysley <evan@kysley.com>
2019-12-20 18:32:14 -0500

    chore: update socket url

--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 
 const socketUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://api.typvp.xyz:8086'
+    ? 'https://api.typvp.xyz'
     : 'http://localhost:8086'
 
 export const socket = io(socketUrl)
