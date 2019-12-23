acd5990db18f974ba96d3f1687030793c025f505
Evan Kysley <evan@kysley.com>
2019-12-22 23:56:09 -0500

    chore: refactor out inline styling

--- src/components/Race/RacePosition.tsx
+++ src/components/Race/RacePosition.tsx
@@ -1,43 +1,63 @@
 import React, {FC} from 'react'
 import {observer} from 'mobx-react-lite'
+import styled from 'styled-components'
 
 import {useStore} from '@/stores'
 
 const colors = ['#FF008C', '#D309E1', '#9C1AFF', '#7700FF']
 
+const PlayerContainer = styled.div`
+  display: flex;
+  align-self: center;
+  max-width: 900px;
+  width: 900px;
+`
+
+const PlayerList = styled.ul`
+  ::before {
+    content: '';
+    display: block;
+    width: 900px;
+    position: absolute;
+    height: 32px;
+    top: 0;
+    left: 0;
+    border-radius: 4px;
+    background: ${({theme}) => theme.backgrounds.secondary};
+  }
+  transition: 1.1s all linear;
+  position: relative;
+  padding: 0;
+  list-style: none;
+`
+
+const PlayerBar = styled.li`
+  height: 32px;
+  margin-bottom: 1em;
+  display: flex;
+  align-items: center;
+  padding-left: 1em;
+  color: white;
+  border-radius: 4px;
+  transition: 1.1s all linear;
+  position: relative;
+`
+
 const RacePosition: FC = observer(({id}: any) => {
   const {RaceStore} = useStore()
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignSelf: 'center',
-        maxWidth: '900px',
-        width: '900px',
-      }}
-    >
-      <ul
+    <PlayerContainer>
+      <PlayerList
         style={{
           width: `${100 - (RaceStore.room!.secondsRemaining / 60) * 100}%`,
-          listStyle: 'none',
-          padding: 0,
-          transition: '1.1s all linear',
         }}
       >
         {RaceStore.room!.players.map((player: any, idx: any) => (
-          <li
+          <PlayerBar
             style={{
-              height: '32px',
               background: `${colors[idx]}`,
               width: `${(player.wpm / RaceStore.fastestPlayer) * 100}%`,
-              marginBottom: '1em',
-              display: 'flex',
-              alignItems: 'center',
-              paddingLeft: '1em',
-              color: 'white',
-              borderRadius: '4px',
-              transition: '1.1s all linear',
             }}
             key={player.id}
           >
@@ -46,10 +66,10 @@ const RacePosition: FC = observer(({id}: any) => {
               {player.wpm}
               {player.id === id && ' (you)'}
             </span>
-          </li>
+          </PlayerBar>
         ))}
-      </ul>
-    </div>
+      </PlayerList>
+    </PlayerContainer>
   )
 })
 
