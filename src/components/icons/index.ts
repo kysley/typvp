c62acd59d9f153abdcacbeb21c80ce6c991bd777
Evan Kysley <evan@kysley.com>
2020-01-16 12:32:08 -0500

    feat: add Save icon & update impl specs

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,5 @@
+import styled from 'styled-components'
+
 export {LoginIcon} from './Login'
 export {LogoutIcon} from './Logout'
 export {RefreshIcon} from './Refresh'
@@ -8,3 +10,15 @@ export {AdjustIcon} from './Adjust'
 export {StopwatchIcon} from './Stopwatch'
 export {UsersIcon} from './Users'
 export {DeviceIcon} from './Device'
+export {SaveIcon} from './Save'
+
+export const SVGDefault = styled.svg`
+  height: 1.5em;
+  cursor: pointer;
+  fill: ${({theme}) => theme.colors.text};
+  margin-right: 0.5em;
+
+  :only-child {
+    margin-right: 0;
+  }
+`
