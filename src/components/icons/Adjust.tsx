ce076db2bfc100f97782b24f68f99de44cff05a0
Evan Kysley <evan@kysley.com>
2020-01-23 12:36:24 -0500

    chore: migrate icons to shared style

--- src/components/icons/Adjust.tsx
+++ src/components/icons/Adjust.tsx
@@ -1,16 +1,9 @@
 import React, {FC} from 'react'
-import styled from 'styled-components'
 
-const SVG = styled.svg`
-  /* right: 1vw; */
-  /* width: 2em; */
-  height: 1.5em;
-  cursor: pointer;
-  fill: ${({theme}) => theme.colors.text};
-`
+import {SVGDefault} from './'
 
 export const AdjustIcon: FC = () => (
-  <SVG viewBox="0 0 24 24">
+  <SVGDefault viewBox="0 0 24 24">
     <path
       d="M17 7A7.74 7.74 0 107 17 7.74 7.74 0 1017 7zm-1.6 1.6a6.12
            6.12 0 01.11 1.14 5.92 5.92 0 01-.16 1.34l-2.44-2.42a5.92 5.92 0
@@ -21,5 +14,5 @@ export const AdjustIcon: FC = () => (
            01-1.09-.07zM14.25 20a5.77 5.77 0 01-4.75-2.51h.25a7.76 7.76 0
            007.75-7.74V9.5A5.75 5.75 0 0114.25 20z"
     />
-  </SVG>
+  </SVGDefault>
 )
