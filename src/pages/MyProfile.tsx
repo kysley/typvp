47ffa18e1ea68e485deded5ed2680ca1feee5739
Evan Kysley <evan@kysley.com>
2020-04-18 01:24:06 -0400

    refactor: profile data flow

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -16,13 +16,23 @@ import {
   ResultValue,
   ResultFilter,
 } from '@/styled/MyProfile'
-import {MY_RESULTS} from '@/graphql/queries'
+import {ACCOUNT_RESULTS} from '@/graphql/queries'
 import Pagination from '@/components/Pagination'
 import {PageHeader} from '@/styled/Theme'
 import {Bubble} from '@/components/Bubble'
 import {Banner} from '@/components/Banner'
 import InlineEditable from '@/components/InlineEditable'
-import {UPDATE_COLOR} from '@/graphql/mutations'
+import {UPDATE_ACCOUNT} from '@/graphql/mutations'
+import {
+  UpdateAccountMutation,
+  MutationUpdateAccountArgs,
+  AccountResultsQueryVariables,
+  OrderByArg,
+  AccountResultsQuery,
+  ResultType,
+  Test,
+} from '@/generated/graphql'
+import {client} from '@/services/Client'
 
 function normalizeHexCode(hexCode: string) {
   if (!hexCode.startsWith('#')) hexCode = `#${hexCode}`
@@ -32,50 +42,62 @@ function normalizeHexCode(hexCode: string) {
 
 const MyProfile: FC = observer(() => {
   const {UserStore} = useStore()
-  const [pagination, setPagination] = useState({
+  const [pagination, setPagination] = useState<AccountResultsQueryVariables>({
     first: 15,
     skip: 0,
-    date: 'createdAt_DESC',
-    type: '',
+    orderBy: {createdAt: OrderByArg.Desc},
+    where: {},
+    id: '',
   })
-  const [result] = useQuery({
-    query: MY_RESULTS,
-    requestPolicy: 'cache-first',
-    variables: {
-      ...pagination,
-    },
+  const [results, setResults] = useState<{
+    count: number
+    results: Partial<Test>[]
+  }>({
+    count: 0,
+    results: [],
   })
 
-  const [, execMutation] = useMutation(UPDATE_COLOR)
+  const [, execMutation] = useMutation<UpdateAccountMutation>(UPDATE_ACCOUNT)
 
   useEffect(() => {
-    if (result.data) {
-      const {
-        data: {
-          me: {filterResults},
-        },
-      } = result
-      // console.log(me)
-      if (UserStore.me) {
-        // UserStore.me!.results = results
-        if (
-          pagination.type === '' &&
-          filterResults.length > (UserStore.me!.testCount || 0)
-        ) {
-          UserStore.me!.testCount = filterResults.length
+    let didCancel = false
+
+    async function fetch(id: string) {
+      const res = await client
+        .query<AccountResultsQuery>(ACCOUNT_RESULTS, {
+          ...pagination,
+          id,
+        } as AccountResultsQueryVariables)
+        .toPromise()
+
+      if (!didCancel) {
+        if (res.data) {
+          const {count, results} = res.data.accountResults
+          setResults({count, results})
         }
       }
     }
-  }, [result.data])
+
+    console.log(UserStore.me)
+    if (UserStore.me) {
+      fetch(UserStore.me.id!)
+    }
+
+    return () => {
+      didCancel = true
+    }
+  }, [UserStore.me, pagination])
 
   const handleColorUpdate = async (colorCallback: string) => {
-    const {
-      data: {updateAccountColor: account},
-    } = await execMutation({
-      color: normalizeHexCode(colorCallback),
-    })
-    console.log(account)
-    UserStore.me = {...UserStore.me, ...account} as any
+    const res = await execMutation({
+      data: {
+        color: normalizeHexCode(colorCallback),
+      },
+    } as MutationUpdateAccountArgs)
+
+    if (res.data) {
+      UserStore.me = {...UserStore.me, ...res.data.updateAccount}
+    }
   }
 
   if (UserStore.me === undefined && !UserStore.fetchingUser) {
@@ -102,7 +124,7 @@ const MyProfile: FC = observer(() => {
               </div>
               <div>
                 <ProfileHeader>tests taken</ProfileHeader>
-                <ProfileValue>{UserStore.me.testCount || 'n/a'}</ProfileValue>
+                {/* <ProfileValue>{UserStore.me.testCount || 'n/a'}</ProfileValue> */}
               </div>
               <div>
                 <ProfileHeader>last seen</ProfileHeader>
@@ -142,35 +164,35 @@ const MyProfile: FC = observer(() => {
                     values={[
                       {
                         name: 'Date Taken (New First)',
-                        value: 'createdAt_DESC',
+                        value: OrderByArg.Desc,
                       },
-                      {name: 'Date Taken (Old First)', value: 'createdAt_ASC'},
+                      {name: 'Date Taken (Old First)', value: OrderByArg.Asc},
                     ]}
                     callback={opt =>
                       setPagination(prev => {
-                        return {...prev, date: opt.value}
+                        return {...prev, orderBy: {createdAt: opt.value}}
                       })
                     }
                   />
                   <label>Filter by:</label>
                   <Bubble
                     values={[
-                      {name: 'All', value: ''},
-                      {name: 'Singleplayer', value: 'SINGLEPLAYER'},
-                      {name: 'Trial', value: 'TRIAL'},
+                      {name: 'All', value: undefined},
+                      {name: 'Singleplayer', value: ResultType.Singleplayer},
+                      {name: 'Trial', value: ResultType.Trial},
                     ]}
                     callback={opt =>
                       setPagination(prev => {
-                        return {...prev, type: opt.value}
+                        return {
+                          ...prev,
+                          where: {...prev.where, type: {equals: opt.value}},
+                        }
                       })
                     }
                   />
                 </div>
                 <Pagination
-                  totalRecords={
-                    (result.data && result.data.me.testCountByType) ||
-                    UserStore.me.testCount
-                  }
+                  totalRecords={results.count}
                   pageLimit={15}
                   pageNeighbours={1}
                   onPageChanged={data => {
@@ -180,33 +202,27 @@ const MyProfile: FC = observer(() => {
                   }}
                 />
               </ResultFilter>
-              {!result.fetching && (
-                <>
-                  {result.data.me.filterResults &&
-                    result.data.me.filterResults.map((result: any) => (
-                      <ResultWrapper key={result.id}>
-                        <p>
-                          {result.type} |{' '}
-                          <TimeAgo datetime={result.createdAt} />
-                        </p>
-                        <ResultHeader>wpm</ResultHeader>
-                        <ResultValue>{result.wpm}</ResultValue>
-                        <ResultHeader>cpm</ResultHeader>
-                        <ResultValue>{result.cpm}</ResultValue>
-                        <ResultHeader>cpm(raw)</ResultHeader>
-                        <ResultValue>{result.rawCpm}</ResultValue>
-                        <ResultHeader>corr</ResultHeader>
-                        <ResultValue>{result.correct}</ResultValue>
-                        <ResultHeader>incorr</ResultHeader>
-                        <ResultValue>{result.incorrect}</ResultValue>
-                        <ResultHeader>crrns</ResultHeader>
-                        <ResultValue>{result.corrections}</ResultValue>
-                        <ResultHeader>cmpltns</ResultHeader>
-                        <ResultValue>{result.wordIndex}</ResultValue>
-                      </ResultWrapper>
-                    ))}
-                </>
-              )}
+              {results.results.map((result: any) => (
+                <ResultWrapper key={result.id}>
+                  <p>
+                    {result.type} | <TimeAgo datetime={result.createdAt} />
+                  </p>
+                  <ResultHeader>wpm</ResultHeader>
+                  <ResultValue>{result.wpm}</ResultValue>
+                  <ResultHeader>cpm</ResultHeader>
+                  <ResultValue>{result.cpm}</ResultValue>
+                  <ResultHeader>cpm(raw)</ResultHeader>
+                  <ResultValue>{result.rawCpm}</ResultValue>
+                  <ResultHeader>corr</ResultHeader>
+                  <ResultValue>{result.correct}</ResultValue>
+                  <ResultHeader>incorr</ResultHeader>
+                  <ResultValue>{result.incorrect}</ResultValue>
+                  <ResultHeader>crrns</ResultHeader>
+                  <ResultValue>{result.corrections}</ResultValue>
+                  <ResultHeader>cmpltns</ResultHeader>
+                  <ResultValue>{result.wordIndex}</ResultValue>
+                </ResultWrapper>
+              ))}
             </ResultsArea>
           </ProfileGrid>
         </>
